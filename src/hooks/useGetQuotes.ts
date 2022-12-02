f17c82cdea84cde4d1a282ea1cbae5793f10bfd0
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-12-02 17:28:41 +0100

    Add custom hook for fetching quotes based on page number

---
+++ src/hooks/useGetQuotes.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { QuoteType } from '../types/QuoteType';
+
+export default function useGetQuotes(page: number) {
+  const [quotes, setQuotes] = useState<QuoteType[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    getQuotes();
+  }, [page]);
+
+  const getQuotes = async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(`http://localhost:8000/quotes/?page=${page}`);
+      const json = await res.json();
+
+      setQuotes(json);
+    } catch (error) {
+      setError(error as string);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { quotes, setQuotes, isLoading, error };
+}
