492b22d8e85d6cdad91d313b9303f89f4bdfcab6
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-12-03 15:41:04 +0100

    update useGetGenders custom hook

--- src/hooks/useGetGenders.ts
+++ src/hooks/useGetGenders.ts
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
-import { GenderType } from '../types/QuoteType';
+import { GenderType } from '../types/GenderType';
 import { QuoteType } from '../types/QuoteType';
 
 export default function useGetGenders(quotes: QuoteType[]) {
   const [genders, setGenders] = useState<GenderType[]>();
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     getGenders();
@@ -13,21 +14,23 @@ export default function useGetGenders(quotes: QuoteType[]) {
   const getGenders = async () => {
     setIsLoading(true);
     try {
-      const genders = await Promise.all(
-        quotes.map(({ a }) =>
-          fetch(`https://api.genderize.io/?name=${a.split(' ')[0]}`).then(
-            (res) => res.json()
-          )
-        )
+      const _genders = await Promise.all(
+        quotes.map(async ({ a }) => {
+          const res = await fetch(
+            `https://api.genderize.io/?name=${a.split(' ')[0]}`
+          );
+          const { gender } = await res.json();
+          return gender;
+        })
       );
 
-      setGenders(genders);
+      setGenders(_genders);
     } catch (error) {
-      console.log(error);
+      setError('Failted to get genders');
     } finally {
       setIsLoading(false);
     }
   };
 
-  return { getGenders, genders, isLoading };
+  return { getGenders, genders, isLoading, error };
 }
