c633120fa4e2da0cb7ca42d199a7c8c2c2586451
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-12-03 16:20:58 +0100

    Get gender emoji based on author name

---
+++ src/components/AuthorGender.tsx
@@ -0,0 +1,10 @@
+import TableCell from '@mui/material/TableCell';
+import useGetGender from '../hooks/useGetGenders';
+import LoadingSpinner from './LoadingSpinner';
+
+export default function AuthorGender({ author }: { author: string }) {
+  const { gender, isLoading } = useGetGender(author);
+
+  if (isLoading) <LoadingSpinner />;
+  return <TableCell>{gender}</TableCell>;
+}
