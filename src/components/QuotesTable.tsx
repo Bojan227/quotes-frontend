9b3a333292c09946235d9f2cb9947ec835138e5f
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-12-04 12:24:22 +0100

    Add table pagination inside a table footer and table row

--- src/components/QuotesTable.tsx
+++ src/components/QuotesTable.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import QuotesHeader from './QuotesHeader';
 import QuotesBody from './QuotesBody';
-import Box from '@mui/material/Box';
 import useGenerateNewQuotes from '../hooks/useGenerateNewQuotes';
 import useGetQuotes from '../hooks/useGetQuotes';
 import LoadingSpinner from './LoadingSpinner';
@@ -12,6 +11,10 @@ import {
   TableContainer,
   TablePagination,
   Button,
+  Box,
+  Paper,
+  TableFooter,
+  TableRow,
 } from '@mui/material';
 
 export default function QuotesTable() {
@@ -29,41 +32,47 @@ export default function QuotesTable() {
   if (isLoading) <LoadingSpinner />;
 
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      flexDirection="column"
-      minHeight="100vh"
-      gap="25px"
-    >
-      <TableContainer>
-        <Table>
-          <QuotesHeader />
-          <QuotesBody />
-          <TablePagination
-            rowsPerPageOptions={[10]}
-            count={50}
-            onPageChange={handleChangePage}
-            rowsPerPage={10}
-            page={page}
-          />
-        </Table>
-      </TableContainer>
-      <Box display="flex" gap="25px">
-        <Link to="/random-quote">
-          <Button variant="outlined">Get Random Quote</Button>
-        </Link>
-        <Button
-          variant="outlined"
-          onClick={() => {
-            generateNewQuotes();
-          }}
-        >
-          Generate New Quotes
-        </Button>
+    <>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        minHeight="100vh"
+        gap="25px"
+      >
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+            <QuotesHeader />
+            <QuotesBody />
+            <TableFooter sx={{ backgroundColor: '#6366f1' }}>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[10]}
+                  count={50}
+                  onPageChange={handleChangePage}
+                  rowsPerPage={10}
+                  page={page}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+        <Box display="flex" gap="25px">
+          <Link to="/random-quote">
+            <Button variant="outlined">Get Random Quote</Button>
+          </Link>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              generateNewQuotes();
+            }}
+          >
+            Generate New Quotes
+          </Button>
+        </Box>
+        {error && <Typography>{error}</Typography>}
       </Box>
-      {error && <Typography>{error}</Typography>}
-    </Box>
+    </>
   );
 }
